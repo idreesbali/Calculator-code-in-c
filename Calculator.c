8d64f08227bc9a433328bac3f7e0945d06a25d26
Idrees Bali <idreesfakhruddin07@gmail.com>
2026-09-02 05:56:28 +0000

    Initial commit

---
+++ Calculator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    printf("Enter Number 1:");
+    float num1;
+    scanf("%f",&num1);
+    printf("\nEnter Number 2:");
+    float num2;
+    scanf("%f", &num2);
+    
+}
