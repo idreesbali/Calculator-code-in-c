5a071fb0aa78e6d7a00c86018f54ac5c93c50ce2
Idrees Bali <idreesfakhruddin07@gmail.com>
2026-09-02 06:36:13 +0000

    addition code

--- Calculator.c
+++ Calculator.c
@@ -6,5 +6,11 @@ int main() {
     printf("\nEnter Number 2:");
     float num2;
     scanf("%f", &num2);
-    
+    Printf("\nSelect from the Options:\n1. Addition");
+    int ch;
+    switch(ch) {
+        case 1: 
+        float add = num1 + num2;
+        printf("Answer: %.2f", add);
+    }
 }
